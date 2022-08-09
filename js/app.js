0bdef209fda5cfa83cbd7c2866d4ac653e7b0e59
Matthew Alabi <97913059+Mmt4J@users.noreply.github.com>
2022-08-09 11:49:42 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//Javascript for toggling menu
+var navLinks = document.getElementById("nav-links");
+	
+	function showMenu(){
+		navLinks.style.right = '0';
+	}
+
+	function hideMenu(){
+		navLinks.style.right = '-200px';
+	}
