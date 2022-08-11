ae9709825c51eff373d335e99dee36aa3103da15
Mmt4J <alabimatthwe@gmail.com>
2022-08-11 10:56:09 +0100

    Action page domain name changed

--- action/form-handler.php
+++ action/form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'domain';
+$email_from = 'https://mmt4j.github.io/kapposoft-web2';
 
 $email_subject = 'New Form Submission';
 
