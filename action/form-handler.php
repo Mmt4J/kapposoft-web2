0a00dc191bf4c11786ecc21574885c162771fbcf
Mmt4J <alabimatthwe@gmail.com>
2022-08-11 11:12:02 +0100

    space adjusted on the form

--- action/form-handler.php
+++ action/form-handler.php
@@ -20,6 +20,6 @@ $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 
 
-mail($to, $email_subject, $email_body, $headers);
+mail($to,$email_subject,$email_body,$headers);
 
 header("Location: ../contact.html");
